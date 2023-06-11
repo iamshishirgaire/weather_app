a3cf2ba96d91a8735c726360c21956c580effc82
iamshishirgaire <shishirgaire35@gmail.com>
2023-06-11 21:53:42 +0545

    lightTheme

--- lib/main.dart
+++ lib/main.dart
@@ -7,7 +7,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
+      debugShowCheckedModeBanner: true,
       theme: ThemeData(
         brightness: Brightness.dark,
         scaffoldBackgroundColor: Color(0xFF081B25),
