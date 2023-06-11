b5bcb02390b83579029d5bf8255e0449f0484b00
iamshishirgaire <shishirgaire35@gmail.com>
2023-06-11 21:54:17 +0545

    lightTheme

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: true,
       theme: ThemeData(
-        brightness: Brightness.dark,
+        brightness: Brightness.light,
         scaffoldBackgroundColor: Color(0xFF081B25),
       ),
       home: LoadingScreen(),
