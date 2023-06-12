73cba3397168d5643076fec0ac1afc5a7fd265a3
iamshishirgaire <shishirgaire35@gmail.com>
2023-06-12 06:44:34 +0545

    lightTheme

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -9,6 +9,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:weather_app/main.dart';
+//lololol
 //hellp
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
